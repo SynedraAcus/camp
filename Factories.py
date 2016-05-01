56c34ca48c3207b9ddd413be0567566cdb9bb08a
SynedraAcus <alexeymorozov1991@gmail.com>
2016-05-01 21:25:53 +0800

    Removed Atlas code

--- Factories.py
+++ Factories.py
@@ -4,7 +4,6 @@ from kivy.uix.image import Image
 from kivy.core.image import Image as CoreImage
 from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle, BindTexture
-from kivy.atlas import Atlas
 from Map import RLMap, GroundTile, Actor
 
 class ActorWidget(Widget):
@@ -16,13 +15,6 @@ class ActorWidget(Widget):
         self.img = Image(source='Tmp_frame_black.png', size=(64, 64))
         self.add_widget(self.img)
         self.bind(pos=self.update_img)
-        # self.canvas.add(Rectangle(texture=texture))
-        # t = CoreImage('Tmp_frame_black.png').texture
-        # with self.canvas.after:
-        #     BindTexture(texture=texture)
-            # Rectangle(texture=texture, pos=self.pos, size=self.size)
-            # self.rect = Rectangle(texture=texture, size=self.size, pos=self.pos)
-            # self.bind(size=self.update_texture, pos=self.update_texture)
 
     def update_img(self, a, b):
         self.img.pos = self.pos
@@ -33,8 +25,7 @@ class ActorWidget(Widget):
 
 class TileWidgetFactory(object):
     def __init__(self):
-        self.atlas = Atlas('prototiles.atlas')
-        self.atlas_texture=CoreImage('prototiles.png').texture
+        pass
 
     def create_tile_widget(self, tile):
         if tile.passable:
@@ -44,12 +35,8 @@ class TileWidgetFactory(object):
         return tile.widget
 
     def create_actor_widget(self, actor):
-        widget = ActorWidget(texture=self.atlas.textures['PCproto'],
-                                        size = (64, 64),
-                                        size_hint=(None, None))
-        # widget = Image(texture=self.atlas.textures['PC_proto'],
-        #                size=(64,64),
-        #                size_hint=(None, None))
+        widget = ActorWidget(size = (64, 64),
+                             size_hint=(None, None))
         actor.widget = widget
         return widget
 
