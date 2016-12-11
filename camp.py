6ce5a3c93fb2c9683928612143e8259d71700ae1
SynedraAcus <alexeymorozov1991@gmail.com>
2016-12-11 19:07:00 +0800

    README fixes

--- camp.py
+++ camp.py
@@ -949,7 +949,7 @@ class CampApp(App):
     def build(self):
         root = BoxLayout(orientation='vertical')
         self.game_manager = GameManager(map_file='test_level.lvl')
-        self.game_manager.switch_map('twist')
+        self.game_manager.switch_map('entrance')
         self.game_widget = GameWidget(game_manager=self.game_manager,
                                       size=Window.size,
                                       size_hint=(None, None),
